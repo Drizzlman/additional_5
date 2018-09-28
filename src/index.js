26e313ecacfab0cd74eb0b7a79cdaf86f2f466df
Drizzlman <kovalko.alexey@yandex.by>
2018-09-28 22:12:58 +0300

    Task_6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if(str.length % 2 !== 0){
+        return 0;
+    }
+
+    else {
+        for( var i = str.length - 1; i >= 0; i--) {
+            for ( var j = 0; j < bracketsConfig.length; j++) {
+                if (str[i] === bracketsConfig[j][0]) {
+                    str = str.replace(bracketsConfig[j].join(''), '');
+                }
+            }
+        }
+        return str == '';
+
+    }
 }
